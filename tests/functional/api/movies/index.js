bb1d4d8a0e1b40d607d081325d9b81c0e95b2fd6
Maciej Marchel <maciejmarchel14@gmail.com>
2023-11-22 13:52:48 +0000

    Add movie review tested

--- tests/functional/api/movies/index.js
+++ tests/functional/api/movies/index.js
@@ -5,6 +5,7 @@ import api from "../../../../index";  // Express API application
 const expect = chai.expect;
 
 let movie; // NEW LINE
+let numReviews; // NEW LINE
 
 describe('Movies endpoint',  () => {
 
@@ -105,4 +106,65 @@ describe('Movies endpoint',  () => {
             .end(done);
         });
     });
+
+    describe("POST /api/movies/:id/reviews ", () => {
+
+        before(() => {
+          movie = {
+            id: 527774,
+          };
+          return request(api)
+            .get(`/api/movies/${movie.id}/reviews`)
+            .set("Accept", "application/json")
+            .then((res) => {
+              numReviews = res.body.results.length;
+            });
+        });
+
+        
+        describe("For a valid movie id ", () => {
+          it("should return a 201 status and the newly added review", () => {
+            let review = {
+              author: "joebloggs",
+              content: "In a magical land known as Kumandra....",
+            };
+            return request(api)
+              .post(`/api/movies/${movie.id}/reviews`)
+              .send(review)
+              .expect(201)
+              .then((res) => {
+                expect(res.body).to.have.keys([
+                  "author",
+                  "content",
+                  "id",
+                  "created_at",
+                  "updated_at",
+                ]);
+                expect(res.body).to.have.property("author", review.author);
+              });
+          });
+          after(() => {
+            return request(api)
+              .get(`/api/movies/${movie.id}/reviews`)
+              .set("Accept", "application/json")
+              .then((res) => {
+                expect(res.body.results.length).equals(numReviews + 1);
+              });
+          });
+        });
+
+
+        describe("For an invalid movie id ", () => {
+          it("should return a 404 status and the corresponding error message", () => {
+            return request(api)
+              .post(`/api/movies/99999/reviews`)
+              .send({})
+              .expect(404)
+              .expect({
+                message: "The resource you requested could not be found.",
+                status_code: 404,
+              });
+          });
+        });
+      });
 });
