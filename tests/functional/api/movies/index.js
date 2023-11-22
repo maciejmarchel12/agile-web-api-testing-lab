1605bd6e054848938018f5c955d6b51473d159b7
Maciej Marchel <maciejmarchel14@gmail.com>
2023-11-22 13:42:36 +0000

    Get specific movie tested.

--- tests/functional/api/movies/index.js
+++ tests/functional/api/movies/index.js
@@ -4,7 +4,11 @@ import api from "../../../../index";  // Express API application
 
 const expect = chai.expect;
 
+let movie; // NEW LINE
+
 describe('Movies endpoint',  () => {
+
+    // FIRST TEST
     describe("Movies endpoint", () => {
         describe("GET /api/movies ", () => {
           it("returns all the movies and a status 200", (done) => {
@@ -17,6 +21,42 @@ describe('Movies endpoint',  () => {
                 expect(res.body.results).to.be.a("array");
                 expect(res.body.results.length).to.equal(4);
                 done();
+              });
+          });
+        });
+      });
+
+      //2ND TEST
+      describe("GET /api/movies/:id", () => {
+        before(() => {
+          movie = {
+            id: 527774,
+            title: "Raya and the Last Dragon",
+          };
+        });
+        describe("when the id is valid", () => {
+          it("returns the matching movie", () => {
+            return request(api)
+              .get(`/api/movies/${movie.id}`)
+              .set("Accept", "application/json")
+              .expect("Content-Type", /json/)
+              .expect(200)
+              .then((res) => {
+                expect(res.body).to.have.property("title", movie.title);
+              });
+          });
+        });
+
+        //3RD TEST
+        describe("when the id is invalid", () => {
+          it("returns the NOT found error message", () => {
+            return request(api)
+              .get("/api/movies/9999")
+              .set("Accept", "application/json")
+              .expect("Content-Type", /json/)
+              .expect({
+                message: "The resource you requested could not be found.",
+                status_code: 404,
               });
           });
         });
